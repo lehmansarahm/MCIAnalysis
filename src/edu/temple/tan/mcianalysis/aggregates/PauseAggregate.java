fd2fec48c059befc9b4ad7cbcb01f0fb1299fed1
Sarah Lehman <tug46038@temple.edu>
2017-05-22 14:03:39 -0400

    Tweaks

--- src/edu/temple/tan/mcianalysis/aggregates/PauseAggregate.java
+++ src/edu/temple/tan/mcianalysis/aggregates/PauseAggregate.java
@@ -4,12 +4,13 @@ import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 
 import edu.temple.tan.mcianalysis.utils.Constants;
+import edu.temple.tan.mcianalysis.utils.Constants.PAUSE_AGGREGATE_COLUMN_ORDER;
+import edu.temple.tan.mcianalysis.utils.Constants.PAUSE_OUTPUT_FILE_TOTALS_COLUMN_ORDER;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -50,42 +51,45 @@ public class PauseAggregate {
             //if the file is a directory we want to go into it and aggregate the direction changes
             if (listOfFiles[i].isDirectory()) {
                 File[] innerFiles = listOfFiles[i].listFiles();
-                String total_write_line[] = new String[4];
+                String writeLine[] = new String[PAUSE_AGGREGATE_COLUMN_ORDER.values().length];
                 
-                String writer_path = listOfFiles[i].getAbsolutePath();
-                writer_path = writer_path.concat("/" + aggregateFileName);
-                CSVWriter writer = new CSVWriter(new FileWriter(writer_path));
+                String writerFilePath = listOfFiles[i].getAbsolutePath();
+                writerFilePath = writerFilePath.concat("/" + aggregateFileName);
+                CSVWriter writer = new CSVWriter(new FileWriter(writerFilePath));
 
-                total_write_line[Constants.PAUSE_AGGREGATE_COLUMN_ORDER.TASK.ordinal()] = "Task:";
-                total_write_line[Constants.PAUSE_AGGREGATE_COLUMN_ORDER.NUM_OF_PAUSES.ordinal()] = "Number of Pauses:";
-                total_write_line[Constants.PAUSE_AGGREGATE_COLUMN_ORDER.TOTAL_TIME_PAUSED.ordinal()] = "Total Time Paused:";
-                total_write_line[Constants.PAUSE_AGGREGATE_COLUMN_ORDER.AVG_PAUSE_DURATION.ordinal()] = "Average Pause Duration:";
-
-                writer.writeNext(total_write_line);
+                writeLine[PAUSE_AGGREGATE_COLUMN_ORDER.TASK.ordinal()] = "Task:";
+                writeLine[PAUSE_AGGREGATE_COLUMN_ORDER.NUM_OF_PAUSES.ordinal()] = "Number of Pauses:";
+                writeLine[PAUSE_AGGREGATE_COLUMN_ORDER.TOTAL_TIME_PAUSED.ordinal()] = "Total Time Paused (ms):";
+                writeLine[PAUSE_AGGREGATE_COLUMN_ORDER.AVG_PAUSE_DURATION.ordinal()] = "Average Pause Duration (ms):";
+                writer.writeNext(writeLine);
 
                 //loop through the inner directory
                 for (int j = 0; j < innerFiles.length; j++) {
                     if (!innerFiles[j].getName().equals(aggregateFileName)) {
-                        String[] name_components = innerFiles[j].getName().split("_");
-                        String task_name = (name_components.length >= 3) ? name_components[3] : "";
+                        String[] fileNameComponents = innerFiles[j].getName().split("_");
+                        String taskName = (fileNameComponents.length >= 3) ? fileNameComponents[3] : "";
                         
-                        String full_file_path = innerFiles[j].getAbsolutePath();
-                        CSVReader reader = new CSVReader(new FileReader(full_file_path), ',', '"', 0);
-                        List<String[]> read_all = reader.readAll();
+                        String filePath = innerFiles[j].getAbsolutePath();
+                        CSVReader reader = new CSVReader(new FileReader(filePath), ',', '"', 0);
+                        List<String[]> fileContents = reader.readAll();
                         
                         int m = 0;
-                        while(m < read_all.size() && !read_all.get(m)[0].equalsIgnoreCase("Configuration File Used:")) {
+                        while(m < fileContents.size() && !fileContents.get(m)[0].equalsIgnoreCase("Configuration File Used:")) {
                             m++;
                         }
                         
-                        total_write_line[0] = task_name;
-                        total_write_line[1] = read_all.get(m-1)[1];
-                        total_write_line[2] = read_all.get(m-1)[3];
-                        total_write_line[3] = read_all.get(m-1)[5];
-                        writer.writeNext(total_write_line);
+                        writeLine[PAUSE_AGGREGATE_COLUMN_ORDER.TASK.ordinal()] = taskName;
+                        writeLine[PAUSE_AGGREGATE_COLUMN_ORDER.NUM_OF_PAUSES.ordinal()] = 
+                        		fileContents.get(m-1)[PAUSE_OUTPUT_FILE_TOTALS_COLUMN_ORDER.PAUSE_NUM_VALUE.ordinal()];
+                        writeLine[PAUSE_AGGREGATE_COLUMN_ORDER.TOTAL_TIME_PAUSED.ordinal()] = 
+                        		fileContents.get(m-1)[PAUSE_OUTPUT_FILE_TOTALS_COLUMN_ORDER.TOTAL_PAUSE_TIME_VALUE.ordinal()];
+                        writeLine[PAUSE_AGGREGATE_COLUMN_ORDER.AVG_PAUSE_DURATION.ordinal()] = 
+                        		fileContents.get(m-1)[PAUSE_OUTPUT_FILE_TOTALS_COLUMN_ORDER.AVG_PAUSE_TIME_VALUE.ordinal()];
+                        writer.writeNext(writeLine);
                         reader.close();
                     }
                 }
+                
                 writer.flush();
                 writer.close();
             }
